63150abdd6d4eb1479d1b110d069dc643a1933cd
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-08-27 11:01:41 -0500

    comma and and

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if(len(items)==1):
+        return items[0]
+    elif(len(items)==2):
+        return items[0]+' and '+items[1]
+    else:
+        without_end = [item for item in items if items.index(item)!=len(items)-1]
+        return ', '.join(without_end)+', and '+items[-1]
+
+oxford_comma(['hello', 'bye', 'goodbye'])
